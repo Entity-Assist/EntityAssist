d43c3d23e02302bb4c01d547b54ac7d378db88a8
Marc Magon <ged_marc@hotmail.com>
2017-10-10 02:16:50 +0200

    0.11.7.0. Updates and Sonarqubing

--- src/za/co/mmagon/entityassist/CoreEntity.java
+++ src/za/co/mmagon/entityassist/CoreEntity.java
@@ -23,6 +23,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -575,6 +576,18 @@ public abstract class CoreEntity<J extends CoreEntity<J, Q, I>, Q extends QueryB
 			else if (columnValue instanceof Long)
 			{
 				insertString += columnValue + ", ";
+			}
+			else if (columnValue instanceof Integer)
+			{
+				insertString += columnValue + ", ";
+			}
+			else if (columnValue instanceof BigInteger)
+			{
+				insertString += ((BigInteger) columnValue).longValue() + ", ";
+			}
+			else if (columnValue instanceof BigDecimal)
+			{
+				insertString += ((BigDecimal) columnValue).doubleValue() + ", ";
 			}
 			else if (columnValue instanceof Short)
 			{
