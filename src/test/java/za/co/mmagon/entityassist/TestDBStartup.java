9f186400c976d6fff184e92b5bdc7aea3947d21d
Marc Magon <ged_marc@hotmail.com>
2018-01-22 00:21:13 +0200

    JavaDoc Corrections

--- src/test/java/za/co/mmagon/entityassist/TestDBStartup.java
+++ src/test/java/za/co/mmagon/entityassist/TestDBStartup.java
@@ -2,14 +2,16 @@ package za.co.mmagon.entityassist;
 
 import com.google.inject.Inject;
 import com.google.inject.persist.PersistService;
+import za.co.mmagon.guiceinjection.db.DBStartupAsync;
 
 import javax.sql.DataSource;
 
-public class TestDBStartup extends za.co.mmagon.guiceinjection.db.DBStartupAsync
+public class TestDBStartup extends DBStartupAsync
 {
 	@Inject
 	public TestDBStartup(@TestEntityAssistCustomPersistenceLoader PersistService ps, @TestEntityAssistCustomPersistenceLoader DataSource ds)
 	{
+		super(ps, ds);
 		ps.start();
 	}
 }
