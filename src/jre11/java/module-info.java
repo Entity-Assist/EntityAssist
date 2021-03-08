463606e101d6d24cec50acefdf961021df3319e9
GedMarc <ged_marc@hotmail.com>
2021-03-08 11:11:04 +0200

    Jakarta.Servlet module name

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -24,7 +24,7 @@ module com.entityassist {
 	requires java.naming;
 	requires java.sql;
 
-	requires static java.servlet;
+	requires static jakarta.servlet;
 	requires jakarta.persistence;
 	requires jakarta.xml.bind;
 	
